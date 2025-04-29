d1cf75da007c3e3e93254d32d50bffa8c47d8feb
Olena Hatala <hatala.or@gmail.com>
2025-04-29 17:05:46 +0300

    rename folder

--- app/app.py
+++ app/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-home_page = st.Page("pages/home.py", title="Home", icon=":material/home:")
-basic_simulation_page = st.Page("pages/basic_simulation.py", title="First Simulation", icon=":material/play_circle:")
-complex_simulation_page = st.Page("pages/complex_simulation.py", title="Complex Simulation", icon=":material/analytics:")
+home_page = st.Page("views/home.py", title="Home", icon=":material/home:")
+basic_simulation_page = st.Page("views/basic_simulation.py", title="First Simulation", icon=":material/play_circle:")
+complex_simulation_page = st.Page("views/complex_simulation.py", title="Complex Simulation", icon=":material/analytics:")
 
 simulation_pages = [basic_simulation_page, complex_simulation_page]
 
