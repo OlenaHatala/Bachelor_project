cc532d804fa596c221158c108f8463499ade6e6a
Olena Hatala <hatala.or@gmail.com>
2025-05-25 21:17:34 +0300

    add Trying page and notebooks

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# temp = st.Page("views/Trying.py", title="TEMP")
+temp = st.Page("views/Trying.py", title="TEMP")
 home_page = st.Page("views/home.py", title="Home", icon=":material/home:")
 basic_simulation_page = st.Page("views/basic_simulation.py", title="First Simulation", icon=":material/play_circle:")
 complex_simulation_page = st.Page("views/complex_simulation.py", title="Complex Simulation", icon=":material/analytics:")
@@ -11,7 +11,7 @@ simulation_pages = [basic_simulation_page, complex_simulation_page]
 pg = st.navigation({
     "": [home_page],
     "Simulations": simulation_pages, 
-    # "TRYING TEMP": [temp]
+    "TRYING TEMP": [temp]
     })
 
 pg.run()
