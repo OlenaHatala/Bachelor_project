919ed40370544bae989106973a474e60a8e27136
Olena Hatala <hatala.or@gmail.com>
2025-06-02 02:38:10 +0300

    remove unnecessary files

--- app/app.py
+++ app/app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-# temp = st.Page("views/Trying.py", title="TEMP")
 home_page = st.Page("views/home.py", title="Головна", icon=":material/home:")
-# basic_simulation_page = st.Page("views/basic_simulation.py", title="First Simulation", icon=":material/play_circle:")
-# complex_simulation_page = st.Page("views/complex_simulation.py", title="Complex Simulation", icon=":material/analytics:")
-
-# simulation_pages = [basic_simulation_page, complex_simulation_page]
 
 single_info_simulation = st.Page("views/single_message_spread.py", title="Однорідні джерела", icon=":material/record_voice_over:")
 antagonistic_scrs = st.Page("views/antagonistic_sources.py", title="Інформаційне протистояння", icon=":material/bolt:")
@@ -13,8 +8,6 @@ antagonistic_scrs = st.Page("views/antagonistic_sources.py", title="Інформ
 pg = st.navigation({
     "": [home_page],
     "Симуляції": [single_info_simulation, antagonistic_scrs],
-    # "Simulations": simulation_pages, 
-    # "----------issues----------": [basic_simulation_page, temp]
     })
 
 pg.run()
